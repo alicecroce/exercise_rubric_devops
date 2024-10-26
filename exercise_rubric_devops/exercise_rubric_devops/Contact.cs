0d2805a0c678a5637a465a4175526fca432d1bdf
alicecroce <alicecroce2@gmail.com>
2024-10-26 12:45:21 +0200

    ShowContact method creation

--- exercise_rubric_devops/exercise_rubric_devops/Contact.cs
+++ exercise_rubric_devops/exercise_rubric_devops/Contact.cs
@@ -3,10 +3,17 @@
     internal class Contact
     {
         public string Name { get; set; }
+        public string LastName { get; set; }
 
-        public Contact(string name)
+        public Contact(string name, string lastName)
         {
             Name = name;
+            LastName = lastName;    
+        }
+
+        public string ShowContact(string name, string lastName)
+        {
+            return $"First name: {Name} | Last name: {LastName} ";
         }
     }
 
