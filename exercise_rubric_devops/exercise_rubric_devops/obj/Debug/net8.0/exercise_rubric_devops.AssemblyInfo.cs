c568ebd41ba7f8078cdd0228bfc4a90fa270779b
alicecroce <alicecroce2@gmail.com>
2024-10-26 15:07:20 +0200

    Program Exit: Created

--- exercise_rubric_devops/exercise_rubric_devops/obj/Debug/net8.0/exercise_rubric_devops.AssemblyInfo.cs
+++ exercise_rubric_devops/exercise_rubric_devops/obj/Debug/net8.0/exercise_rubric_devops.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise_rubric_devops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d2805a0c678a5637a465a4175526fca432d1bdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d7bef26c1f2213c5f4ada3fe92ca37a49c7799a")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise_rubric_devops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise_rubric_devops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
