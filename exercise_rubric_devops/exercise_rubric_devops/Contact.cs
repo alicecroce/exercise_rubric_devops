5fb75f2bbade7f0d3b89a4168441f341f8f85b5d
alicecroce <alicecroce2@gmail.com>
2024-10-26 12:41:13 +0200

    Contact Class Creation

---
+++ exercise_rubric_devops/exercise_rubric_devops/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace exercise_rubric_devops
+{
+    internal class Contact
+    {
+        public string Name { get; set; }
+
+        public Contact(string name)
+        {
+            Name = name;
+        }
+    }
+
+    
+}
